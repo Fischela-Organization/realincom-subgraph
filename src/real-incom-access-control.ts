18eaad20c0e27bf82a2504fa7eed426102594966
Norbert Frank Mba <norbertmbafrank@gmail.com>
2022-12-02 19:16:40 +0100

    fixed entity error

--- src/real-incom-access-control.ts
+++ src/real-incom-access-control.ts
@@ -2,24 +2,24 @@ import {
   AddressManagerUpdated as AddressManagerUpdatedEvent,
   AdminCreated as AdminCreatedEvent
 } from "../generated/RealIncomAccessControl/RealIncomAccessControl"
-import { AddressManagerUpdated, AdminCreated } from "../generated/schema"
+import { User } from "../generated/schema"
 
 export function handleAddressManagerUpdated(
   event: AddressManagerUpdatedEvent
 ): void {
-  let entity = new AddressManagerUpdated(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.addressManagerAddress = event.params.addressManagerAddress
-  entity.innitiator = event.params.innitiator
-  entity.save()
+  // let entity = new AddressManagerUpdated(
+  //   event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  // )
+  // entity.addressManagerAddress = event.params.addressManagerAddress
+  // entity.innitiator = event.params.innitiator
+  // entity.save()
 }
 
 export function handleAdminCreated(event: AdminCreatedEvent): void {
-  let entity = new AdminCreated(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.admin = event.params.admin
-  entity.isAdmin = event.params.isAdmin
-  entity.save()
+  // let entity = new AdminCreated(
+  //   event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  // )
+  // entity.admin = event.params.admin
+  // entity.isAdmin = event.params.isAdmin
+  // entity.save()
 }
